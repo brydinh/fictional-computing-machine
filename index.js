3359f9dbcb63782dbab4ffd79eb8fc8dc28302e9
Brian Dinh <bdinh98@gmail.com>
2020-08-05 13:14:20 -0400

    Add conflict check

--- index.js
+++ index.js
@@ -4,16 +4,39 @@ const pool = require("./db");
 
 app.use(express.json());
 
-// get all configs
+async function isRangeGood(minFloat, maxFloat, key1, key2) {
+  const config = await pool.query("SELECT EXISTS(SELECT 1 FROM configuration WHERE key1 = ($1) " +
+    " AND key2 = ($2) AND minFloat <= ($3) AND maxFloat >= ($4))",
+    [key1, key2, maxFloat, minFloat]);
 
-// get a configs
+  return config.rows;
+}
+
+// get all configs
+app.get("/configs", async (req, res) => {
+  try {
+    const configs = await pool.query("SELECT * FROM configuration");
+    console.log(configs.rows);
+
+    res.json(configs.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// get a config
+app.get("/configs/:id", async (req, res) => {
+  try {
+    const config = await pool.query("SELECT * FROM configuration WHERE config_id = ($1)", [req.params.id]);
+    res.json(config.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+})
 
 // insert a config
-
 app.post("/configs", async (req, res) => {
   try {
-    // perform conflict check
-
     const {
       key1,
       key2,
@@ -22,12 +45,19 @@ app.post("/configs", async (req, res) => {
       value
     } = req.body;
 
-    const newConfig = await pool.query("INSERT INTO configuration " +
-      "(key1, key2, minFloat, maxFloat, value) VALUES ($1, $2, $3, $4, $5)" +
-      " RETURNING *", [key1, key2, minFloat, maxFloat, value]);
+    // perform conflict check
+    isRangeGood(minFloat, maxFloat, key1, key2).then(async (result) => {
+      const [{exists}] = result;
 
-    res.json(newConfig);
-
+      if (!exists) {
+        const newConfig = await pool.query("INSERT INTO configuration " +
+          "(key1, key2, minFloat, maxFloat, value) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+          [key1, key2, minFloat, maxFloat, value]);
+        res.json(newConfig.rows[0]);
+      } else {
+        res.send("Already exists");
+      }
+    });
   } catch (err) {
     console.error(err.message);
   }
