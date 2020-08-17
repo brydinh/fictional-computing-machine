9311e17d39f8fbab8fe20002db5c4e804f64fd5f
Brian Dinh <bdinh98@gmail.com>
2020-08-17 16:59:51 -0400

    Add grouping logic & edge case for tuple length

--- importConfig.js
+++ importConfig.js
@@ -2,29 +2,54 @@ const fs = require("fs");
 const readline = require("readline");
 const pool = require("./db/db");
 
-// let key1 = "";
-// let key2 = "";
-// let minFloat = "";
-// let maxFloat = "";
 let value = "";
-let num = 0;
+let lnum = 0;
 let keyLists = [];
+let test = new Map();
+
+function makeTuple(str) {
+  return str
+    .replace(/\s/g, "")
+    .split("),(")
+    .map(el => [...el.replace(/[\()\]]/g, '').split(',')]);
+}
+
+function groupEntries(lst) {
+  lst.sort();
+
+  let groupings = [];
+  let range = lst[0] + .02;
+  let indx = 0;
+
+  groupings.push([]);
+
+  lst.forEach(function(flt){
+    if(flt <= range){
+      groupings[indx].push(flt);
+    } else {
+      range = flt + .02;
+      groupings.push([flt]);
+      indx++;
+    }
+  });
+
+  return groupings;
+}
 
 const lineReader = readline.createInterface({
-  input: fs.createReadStream("importExample.txt")
+  input: fs.createReadStream("importExample.cfg")
 });
 
 // TODO: Account for edge cases & numerous values case
 lineReader.on('line', function(line) {
-  if (num == 0) {
+  if (lnum == 0) {
     var newVal = line.match(/\[(.*?)\]/);
 
     if (newVal) {
       value = newVal[1];
-      console.log(value);
     }
 
-  } else if (num == 1) {
+  } else if (lnum == 1) {
     keys = line.split(' = ').slice(1)[0].split(/\,\s?(?![^\(]*\))/);
 
     keys.forEach(function(a) {
@@ -33,30 +58,47 @@ lineReader.on('line', function(line) {
 
     // console.log(keyLists);
 
+    //TODO: Rewrite key to contain groupings
+    // check to see if entries can be combined into one
     keyLists.forEach(async function(tuple) {
       if (tuple.length == 3) {
         [key1, key2, flt] = tuple;
-        let minFloat = parseFloat(flt) - 0.01;
-        let maxFloat = parseFloat(flt) + 0.01;
 
-        const newConfig = await pool.query("INSERT INTO configuration " +
-          "(key1, key2, minFloat, maxFloat, value) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-          [key1, key2, minFloat, maxFloat, value]);
+        const keyPair = key1 + "_" + key2;
 
-        console.log(newConfig.rows[0]);
+        if (!test.has(keyPair)) {
+          test.set(keyPair, []);
+        }
 
-      } else {
-        console.log(tuple + " man bad");
+        test.get(keyPair).push(parseFloat(flt));
       }
     });
+
+
+    // console.log(test);
+
+
+    console.log(test.values());
   }
-  num++;
+
+  if (lnum < 2) {
+    lnum++;
+  } else {
+    lnum = 0;
+    keyLists = [];
+    test.clear();
+  }
 });
 
 
-function makeTuple(str) {
-  return str
-    .replace(/\s/g, "")
-    .split("),(")
-    .map(el => [...el.replace(/[\()\]]/g, '').split(',')]);
-}
+
+// let minFloat = parseFloat(flt) - 0.01;
+// let maxFloat = parseFloat(flt) + 0.01;
+
+// console.log(key1 + "_" + key2 + ": (" + minFloat + "-" + maxFloat + ") " + value);
+
+// const newConfig = await pool.query("INSERT INTO configuration " +
+//   "(key1, key2, minFloat, maxFloat, value) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+//   [key1, key2, minFloat, maxFloat, value]);
+
+// console.log(newConfig.rows[0]);
